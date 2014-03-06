a0fd69ad127a517b641cdf819d4b627e3887f75e
Michael Pheasant <mpheasant@gmail.com>
2014-03-06 10:49:45 +1000

    import project structure from Om intermediate tutorial https://github.com/swannodette/om/wiki/Intermediate-Tutorial

--- project.clj
+++ project.clj
@@ -4,8 +4,34 @@
   :license {:name "GPL - v 3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"
             :distribution :repo}
+
   :jvm-opts ^:replace ["-Xmx2g" "-server" "-d64" "-Djava.net.preferIPv4Stack=true"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}}
-)
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [ring/ring "1.2.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.5.0"]
+                 [om-sync "0.1.1"]
+                 [compojure "1.1.6"]
+                 [fogus/ring-edn "0.2.0"]
+                 [com.datomic/datomic-free "0.9.4532"]
+                 [ring-mock "0.1.5"]]
+
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [org.clojure/java.classpath "0.2.0"]]}}
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src/clj" "src/cljs"]
+  :resource-paths ["resources"])
+
+  :ring {:handler bubble-time.web/handler}
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/clj" "src/cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true}}]}
--- src/bubble_time/web.clj
+++
@@ -1,5 +0,0 @@
-(ns bubble-time.web)
-
-
-
-
